edee50d1d1387b7de1e42a84906da967191dd6fc
AtomiJD <achrist@computerwelt.de>
2025-07-20 06:42:53 +0200

    before linux

--- jdb/DSPlus_TileA2_ModernExterior.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.2" name="DSPlus_TileA2_ModernExterior" tilewidth="32" tileheight="32" tilecount="192" columns="16">
- <image source="../../media/tilemaps/DSPlus_TileA2_ModernExterior.png" width="512" height="384"/>
-</tileset>
--- jdb/Intern.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.2" name="Intern" tilewidth="32" tileheight="32" tilecount="192" columns="16">
- <image source="../../media/tilemaps/DSPlus_TileA2_ModernInterior.png" trans="ff00ff" width="512" height="384"/>
-</tileset>
